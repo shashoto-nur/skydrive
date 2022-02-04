f76454d18d3017de0021d118a2996f1c9bcb25f1
shashoto-nur <nurulaziz.nz@gmail.com>
2022-02-04 13:56:34 +0600

    mail server initiated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,46 +1,42 @@
+import { useState } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-import logo from './logo.svg';
 import './App.css';
 
 const client = new W3CWebSocket('ws://127.0.0.1:5000');
 
 const App = () => {
 
-  client.onerror = function() {
-      console.log('Connection Error');
-  };
+    client.onerror = function() {
+        console.log('Connection Error');
+    };
 
-  client.onopen = function() {
-      console.log('WebSocket Client Connected');
+    client.onopen = function() {
+        console.log('WebSocket Client Connected');
+    };
 
-      if (client.readyState === client.OPEN) {
-          var number = Math.round(Math.random() * 0xFFFFFF);
-          client.send(number.toString());
-      };
-  };
+    client.onmessage = function(e) {
+        if (typeof e.data === 'string') console.log("Received: '" + e.data + "'");
+    };
 
-  client.onmessage = function(e) {
-      if (typeof e.data === 'string') console.log("Received: '" + e.data + "'");
-  };
+    const [email, setEmail] = useState('Enter your email');
+    const onMailChange = (event: React.ChangeEvent<HTMLInputElement>) => { setEmail(event.target.value); };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const sendEmail = () => {
+        if (client.readyState === client.OPEN && email !== 'Enter your email')
+            client.send(email);
+        else alert('Please enter an email!');
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <form onSubmit={ event => event.preventDefault() }>
+                    <input type="text" name="email" id="email" onChange={ onMailChange } placeholder={ email } />
+                    <button type="submit" onClick={ sendEmail }>Send</button>
+                </form>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,47 @@
+require('dotenv').config();
 import express from 'express';
 import http from 'http';
 import * as WebSocket from 'ws';
 import { AddressInfo } from 'net';
+import { createTransport } from "nodemailer";
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const transporter = createTransport({
+    service: process.env.MAIL_SERVICE,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS
+    }
+});
+
+async function sendMail(mail: string, otp: number) {
+    const mailOptions = {
+        from: process.env.MAIL_USER,
+        to: mail,
+        subject: 'OTP from SkyDrive',
+        text: `Your OTP is ${ otp }`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) console.log(error);
+        else console.log('Email sent: ' + info.response);
+    });
+}
+
 wss.on('connection', (ws: WebSocket) => {
 
-    ws.on('message', (message: string) => {
-        console.log(`Message: ${ message }`);
-        ws.send(`Message: ${ message }`);
+    ws.on('message', (mail: string) => {
+        console.log(`Provided mail address: ${ mail }`);
+
+        const otp = Math.round(Math.random() * 0xFFFFFF);
+        sendMail(mail, otp).catch(console.error);
+
+        ws.send(`OTP sent. Check your email: ${ mail }`);
     });
 
     ws.send('Websocket server online...');
